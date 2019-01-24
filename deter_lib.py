8c102fd026c5bcb0a5a888d35dfb923cf3986845
ACEinfinity7 <acelko7@gmail.com>
2019-01-24 18:20:13 -0500

    added def for 3x3

--- deter_lib.py
+++ deter_lib.py
@@ -12,3 +12,22 @@ def deter2x2(matrix):
     # TODO: calculate the determinant of the 2x2 matrix
 
     return result
+
+
+
+def deter3x3(matrix):
+    a = matrix[0][0]
+    b = matrix[0][1]
+    c = matrix[0][2]
+    d = matrix[1][0]
+    e = matrix[1][1]
+    f = matrix[1][2]
+    g = matrix[2][0]
+    h = matrix[2][1]
+    i = matrix[2][2]
+    result = (a*e*i) - (a*f*h) - (b*d*i) + (b*f*g) + (c*d*h) - (c*e*g)
+    return result
+
+
+matrix_test = [[4,1,3],[5,3,1],[8,4,2]]
+print(deter3x3(matrix_test))
